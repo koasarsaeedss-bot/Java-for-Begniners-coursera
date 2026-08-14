a38f092c4dda9fd62d49f66e751eaf5eae97211c
Kosar saeed <koasarsaeedss@gmail.com>
2026-08-14 15:37:27 +0500

    update

---
+++ Module 2/Lab task/mathsoperators.java
@@ -0,0 +1,13 @@
+public class mathsoperators {
+    public static void main(String[] args) {
+        int a= 10;
+        int b= 20;
+        System.out.println("performing mathematical operators on " +a+"and "+b);
+        System.out.println("addition is "+(a+b));
+        System.out.println("subtraction is "+(a-b));
+        System.out.println("Multiplication is "+ (a*b));
+        // we can use double for more precise division
+        System.out.println("division is "+(a%b));
+
+    }
+}
